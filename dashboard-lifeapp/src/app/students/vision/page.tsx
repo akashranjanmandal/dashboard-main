82d96a7e7f91891b53b0ad4bc2842954cf0a772e
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2025-06-24 04:48:57 +0530

    Update page.tsx of vision for api

--- dashboard-lifeapp/src/app/students/vision/page.tsx
+++ dashboard-lifeapp/src/app/students/vision/page.tsx
@@ -8,8 +8,8 @@ import { Download, Search, XCircle } from 'lucide-react';
 const inter = Inter({ subsets: ['latin'] });
 // const api_startpoint = 'https://lifeapp-api-vv1.vercel.app'
 // const api_startpoint = 'http://152.42.239.141:5000'
-const api_startpoint = 'http://127.0.0.1:5000'
-// const api_startpoint = 'http://152.42.239.141:5000'
+// const api_startpoint = 'http://127.0.0.1:5000'
+ const api_startpoint = 'http://152.42.239.141:5000'
 
 
 interface SessionRow {
