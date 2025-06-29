d162afa1df5e5c976e103c7af5c3efbcdfa8baa0
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2025-06-29 16:18:25 +0530

    Update page.tsx api changes for level

--- dashboard-lifeapp/src/app/settings/levels/page.tsx
+++ dashboard-lifeapp/src/app/settings/levels/page.tsx
@@ -30,7 +30,7 @@
 
     // const api_startpoint = 'https://lifeapp-api-vv1.vercel.app'
     // const api_startpoint = 'http://localhost:5000'
-    const api_startpoint = 'http://152.42.238.141:5000'
+    const api_startpoint = 'http://152.42.239.141:5000'
 
     export default function SettingsLevels() {
         const [levels, setLevels] = useState<Levels[]>([]);
