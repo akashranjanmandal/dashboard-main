81b87aacbae69fb2fa20a53bbc70e4b8084cff84
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2025-06-30 11:23:20 +0530

    Update page.tsx testing push for campaign

--- dashboard-lifeapp/src/app/campaigns/page.tsx
+++ dashboard-lifeapp/src/app/campaigns/page.tsx
@@ -8,8 +8,9 @@ import AddCampaignModal from './AddCampaignModal';
 
 const inter = Inter({ subsets: ['latin'] });
 // const api_startpoint = 'https://lifeapp-api-vv1.vercel.app'
-// const api_startpoint = 'http://localhost:5000';
+// const api_startpoint = 'http://localhost:5000
 const api_startpoint = 'http://152.42.239.141:5000'
+;
 
 interface Campaign {
   id: number;
