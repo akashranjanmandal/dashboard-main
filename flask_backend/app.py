947f5b1d1a34a6c26781d84a8f018700a05f839a
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2025-06-30 11:32:11 +0530

    Update app.py campaign test

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -28,10 +28,8 @@ import uuid
 import boto3
 
 # Load environment variables from .env file
-
 env_path = Path('.') / '.local.env'
 load_dotenv()
-
 # env_path = Path(__file__).resolve().parent / '.local.env'
 # load_dotenv(dotenv_path=env_path)
 # def get_db_connection():
