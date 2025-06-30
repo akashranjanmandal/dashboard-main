311567efe51177334a7dd8d25828685f98c1d92a
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2025-06-30 11:25:18 +0530

    Update AddCampaignModal.tsx test push for campaign

--- dashboard-lifeapp/src/app/campaigns/AddCampaignModal.tsx
+++ dashboard-lifeapp/src/app/campaigns/AddCampaignModal.tsx
@@ -12,7 +12,7 @@ import ReactDOM from 'react-dom';
 const inter = Inter({ subsets: ['latin'] });
 //const api_startpoint = 'https://lifeapp-api-vv1.vercel.app'
 // const api_startpoint = 'http://localhost:5000'
-const api_startpoint = 'http://http://152.42.239.141:5000'
+const api_startpoint = 'http://152.42.239.141:5000'
 
 interface Reference { id: number; title: string; }
 interface PropTypes {
