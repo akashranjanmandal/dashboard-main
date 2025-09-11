e30cbefa0ee317435aae8ce82d41fd897a4b4e5e
Sayandeep.. <134132274+Sayandeep1013@users.noreply.github.com>
2025-09-11 16:28:33 +0530

    Update next.config.ts

--- dashboard-lifeapp/next.config.ts
+++ dashboard-lifeapp/next.config.ts
@@ -1,14 +1,13 @@
-// next.config.js
 const nextConfig = {
   async rewrites() {
     const isProd = process.env.NODE_ENV === 'production';
-    
+
     return [
       {
         source: '/api/:path*',
-        destination: isProd 
-          ? 'http://152.42.239.141:5000/api/:path*'  // Production server
-          : 'http://127.0.0.1:5000/api/:path*',       // Local development
+        destination: isProd
+          ? 'http://152.42.239.141:5000/api/:path*'   // production
+          : 'http://127.0.0.1:5000/api/:path*',       // local dev
       },
     ];
   },
